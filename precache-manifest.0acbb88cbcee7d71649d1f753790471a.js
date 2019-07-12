c7fb7decd145f9110ca82183f5f7f36fe461a510
mygithub <lilycheng84@gmail.com>
2019-07-12 00:41:52 -0500

    Updates

---
+++ precache-manifest.0acbb88cbcee7d71649d1f753790471a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0fbbea33ec3a592b0f0b3bbb1890212",
+    "url": "/weichenglily/index.html"
+  },
+  {
+    "revision": "de29b9d0158c1ce003eb",
+    "url": "/weichenglily/static/css/main.e11a1e25.chunk.css"
+  },
+  {
+    "revision": "8927363ef23c66fb2763",
+    "url": "/weichenglily/static/js/2.4da0bb8d.chunk.js"
+  },
+  {
+    "revision": "de29b9d0158c1ce003eb",
+    "url": "/weichenglily/static/js/main.8c08187f.chunk.js"
+  },
+  {
+    "revision": "2f9474bfe9a4a07c5eb6",
+    "url": "/weichenglily/static/js/runtime~main.ddc9e83c.js"
+  },
+  {
+    "revision": "fffda91225288ea9bb02d505b47db49f",
+    "url": "/weichenglily/static/media/favicon.fffda912.png"
+  },
+  {
+    "revision": "f6e3e0a34d2f6e256ef53ccead8c01aa",
+    "url": "/weichenglily/static/media/wei.f6e3e0a3.jpg"
+  }
+]);
