2d6c105e6844a767905ca404a85153f780d8e652
mygithub <lilycheng84@gmail.com>
2019-07-16 00:31:55 -0500

    Updates

---
+++ precache-manifest.6a9c5d804388e95ff28e0bdd17693f4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f51c091c60e38958239d31d235f9a9d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3344629bae3c90b07f0",
+    "url": "/static/css/main.50e3fc41.chunk.css"
+  },
+  {
+    "revision": "8927363ef23c66fb2763",
+    "url": "/static/js/2.4da0bb8d.chunk.js"
+  },
+  {
+    "revision": "c3344629bae3c90b07f0",
+    "url": "/static/js/main.9b266808.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fffda91225288ea9bb02d505b47db49f",
+    "url": "/static/media/favicon.fffda912.png"
+  },
+  {
+    "revision": "f6e3e0a34d2f6e256ef53ccead8c01aa",
+    "url": "/static/media/wei.f6e3e0a3.jpg"
+  }
+]);
