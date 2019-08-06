9189fc608e78a295637e7e881a3e1170f522c0cb
Wei Cheng <weichenglily@gmail.com>
2019-08-05 23:40:25 -0500

    Updates

---
+++ precache-manifest.21d215693b2ef0cac75abab6ee131369.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac724905766d30e2f7cbf6081c79317",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33aad10defb429aee7b5",
+    "url": "/static/css/main.66d8dca4.chunk.css"
+  },
+  {
+    "revision": "80b26eebf5ff336c6666",
+    "url": "/static/js/2.84483a47.chunk.js"
+  },
+  {
+    "revision": "33aad10defb429aee7b5",
+    "url": "/static/js/main.3553c147.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2568e711fec5c556dba190dc35964d01",
+    "url": "/static/media/qeesi00.2568e711.png"
+  },
+  {
+    "revision": "89fa9ad8836a031302871fe949302d18",
+    "url": "/static/media/recycleAssumption.89fa9ad8.png"
+  },
+  {
+    "revision": "ef4f629df038b5d78829c11edd18b650",
+    "url": "/static/media/recycleBox.ef4f629d.png"
+  },
+  {
+    "revision": "ebd45b0cd6a1fd57f8f29571807e4793",
+    "url": "/static/media/recycleCover.ebd45b0c.png"
+  },
+  {
+    "revision": "6b5635754bf2dcf5b8b8abe5e0bead61",
+    "url": "/static/media/recycleIdeas.6b563575.png"
+  },
+  {
+    "revision": "54921e487ca31552fbb2a04f5a1baae4",
+    "url": "/static/media/recycleInsights.54921e48.png"
+  },
+  {
+    "revision": "49d2cf9794891ef4cb24818ac0e8036a",
+    "url": "/static/media/recycleProcess.49d2cf97.png"
+  },
+  {
+    "revision": "d1bc60a0e08137b20477b1ec73e3b099",
+    "url": "/static/media/recyclePrototype1.d1bc60a0.png"
+  },
+  {
+    "revision": "dc0148cdf582a7ddce7702a771a81563",
+    "url": "/static/media/recyclePrototype2.dc0148cd.png"
+  },
+  {
+    "revision": "ac0e09f7e2fb459dc7abf3211bc1ee27",
+    "url": "/static/media/recycleThemes.ac0e09f7.png"
+  },
+  {
+    "revision": "c4156efcd63ee4cc1aa9408ddb0b100e",
+    "url": "/static/media/wei.c4156efc.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b4a82bc26d1ead2959a068c56e717e35.js"
+  "/precache-manifest.21d215693b2ef0cac75abab6ee131369.js"
 );
 
 self.addEventListener('message', (event) => {
