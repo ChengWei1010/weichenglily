5a1a05207fb50d44658690df630ef5bfa6021a52
Wei Cheng <weichenglily@gmail.com>
2020-12-06 09:28:40 -0500

    revert to early 2020

--- src/components/NavBar/NavItem.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-function NavItem(props){
-    console.log(props.url);
-  return(
-    <a href="{props.url}" target="{props.target}" className="nav-item">{props.title}</a>
-  )
-}
-export default NavItem
